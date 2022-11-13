c11536be27e0b1a202ca9a26fa4b81d35fc1fb79
tazadejava <tonitruirrein@gmail.com>
2022-11-12 23:26:44 -0500

    Create expanding block animation

--- tools/build.sh
+++ tools/build.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+# go to main directory
+cd ../
+
 command=$1
 
 if [[ $command == "test" ]]
 then
     # run personal compiler
-    sed -i '/curr_time_counter == 650000/c\curr_time_counter == 0' test
+    sed -i 's/curr_time_counter == 650000/curr_time_counter == 0/g' src/game_state.sv
+    # end run personal compiler
 
     iverilog -g2012 -o sim/game_logic_and_renderer_tb.out sim/game_logic_and_renderer_tb.sv src/game_logic_and_renderer.sv src/game_state.sv src/block_positions.sv src/state_processor.sv src/renderer.sv && vvp sim/game_logic_and_renderer_tb.out
 elif [[ $command == "build" ]]
 then
     # run personal compiler
-    sed -i '/TEXT_TO_BE_REPLACED/c\This line is removed by the admin.' test
+    sed -i 's/curr_time_counter == 0/curr_time_counter == 650000/g' src/game_state.sv
+    # end run personal compiler
 
-    vivado -mode batch -source build.tcl && penFPGALoader -b arty_a7_100t output_files/final.bit
+    vivado -mode batch -source build.tcl && openFPGALoader -b arty_a7_100t output_files/final.bit
+elif [[ $command == "fpga" ]]
+then
+    openFPGALoader -b arty_a7_100t output_files/final.bit
+else
+    echo "unknown command."
 fi
