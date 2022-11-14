6cd34415570453a543a5e035eb41fd3f9a388a88
tazadejava <tonitruirrein@gmail.com>
2022-11-14 13:03:01 -0500

    Work in progress renderer n blocks

--- tools/build.sh
+++ tools/build.sh
@@ -11,7 +11,7 @@ then
     sed -i 's/curr_time_counter == 650000/curr_time_counter == 10/g' src/game_state.sv
     # end run personal compiler
 
-    iverilog -g2012 -o sim/game_logic_and_renderer_tb.out sim/game_logic_and_renderer_tb.sv src/game_logic_and_renderer.sv src/game_state.sv src/block_positions.sv src/state_processor.sv src/renderer.sv src/block_loader.sv && vvp sim/game_logic_and_renderer_tb.out
+    iverilog -g2012 -o sim/game_logic_and_renderer_tb.out sim/game_logic_and_renderer_tb.sv src/game_logic_and_renderer.sv src/game_state.sv src/block_positions.sv src/block_selector.sv src/state_processor.sv src/renderer.sv src/block_loader.sv && vvp sim/game_logic_and_renderer_tb.out
 elif [[ $command == "build" ]]
 then
     # run personal compiler
